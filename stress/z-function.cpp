204c84d74f34395ae2df96222b776b37b682e677
Leonid Danilevich <58666971+No-Idle@users.noreply.github.com>
2023-12-10 18:18:22 +0300

    Update z-function.cpp

--- stress/z-function.cpp
+++ stress/z-function.cpp
@@ -25,19 +25,16 @@ int32_t main() {
 }
 
 mt19937 rng(0);
-vector<int> z_function(string s) {
-    int n = s.size();
-    vector<int> z(n, n);
-    int l = 0, r = 0;
-    for (int i = 1; i < s.size(); i++) {
-        z[i] = (i > r ? 0 : min(r - i + 1, z[i - l]));
-        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
-            z[i]++;
-        }
-        if (i + z[i] - 1 > r) {
-            l = i;
-            r = i + z[i] - 1;
-        }
+vector<int> z_function (string s) { // z[i] - lcp of s and s[i:]
+    int n = (int) s.length();
+    vector<int> z (n);
+    for (int i=1, l=0, r=0; i<n; ++i) {
+        if (i <= r)
+            z[i] = min (r-i+1, z[i-l]);
+        while (i+z[i] < n && s[z[i]] == s[i+z[i]])
+            ++z[i];
+        if (i+z[i]-1 > r)
+            l = i,  r = i+z[i]-1;
     }
     return z;
 }
@@ -48,7 +45,6 @@ void solve() {
         string s(n, 'a');
         for (char &c : s) c = "abcd"[rng() % 4];
         vector<int> p(n);
-        p[0] = n;
         for (int i = 1; i < n; ++i) {
             while (i + p[i] < n && s[p[i]] == s[i + p[i]]) {
                 p[i]++;
