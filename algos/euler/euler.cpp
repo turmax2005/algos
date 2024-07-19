4cd5cbda65596e85d8415b9adcff506c4aca7d43
No-Idle <leo.danilevich@mail.ru>
2024-07-19 11:38:20 +0300

    fixed euler

--- algos/euler/euler.cpp
+++ algos/euler/euler.cpp
@@ -1,9 +1,5 @@
-vector<int> euler(vector<vector<pair<int, int>>> g) {  // pair{nxt, idx}
+vector<int> euler(vector<vector<pair<int, int>>> g, int src) {  // g[cur][i] = pair{nxt, idx}
   int n = g.size();
-  vector<pair<int, int>> e(p.size());
-  // build graph
-  vector<int> in(n), out(n);
-  for (auto [u, v] : e) in[v]++, out[u]++;
   vector<int> used(m), it(n), cycle;
   auto dfs = [&](auto dfs, int cur) -> void {
     while (true) {
@@ -12,44 +8,11 @@ vector<int> euler(vector<vector<pair<int, int>>> g) {  // pair{nxt, idx}
       auto [nxt, idx] = g[cur][it[cur]];
       used[idx] = true;
       dfs(dfs, nxt);
-      cycle.push_back(idx);
+      cycle.push_back(idx); // or {cur, nxt}
     }
   };
-  int cnt = 0, odd = -1;
-  for (int i = 0; i < n; ++i){
-    if (out[i] && odd == -1) odd = i;
-    if (in[i] != out[i]) {
-      if (in[i] + 1 == out[i]) odd = i;
-      if (abs(in[i] - out[i]) > 1) return {}; // must hold
-      cnt++;
-    }
-  }
-  if (cnt != 0 && cnt != 2) return {}; // must hold
-  // for undirected find odd vertex (and count that # of odd is 0 or 2)
-  dfs(dfs, odd);
+  dfs(dfs, src);
   reverse(cycle.begin(), cycle.end());
-  if (cycle.size() != m) return {};
-  return cycle;
-}
-
-vector<pair<int,int> > euler2(vector<vector<pair<int, int>>> g,vector<pair<int,int> > e) {  // pair{nxt, idx}
-    int m=e.size();
-  int n = g.size();
-  // build graph
-  vector<int> used(m), it(n);vector<pair<int,int> > cycle;
-  auto dfs = [&](auto dfs, int cur) -> void {
-    while (true) {
-      while (it[cur] < g[cur].size() && used[g[cur][it[cur]].second]) it[cur]++;
-      if (it[cur] == g[cur].size()) return;
-      auto [nxt, idx] = g[cur][it[cur]];
-      used[idx] = true;
-      dfs(dfs, nxt);
-      cycle.push_back({cur,nxt});
-    }
-  };
-  // for undirected find odd vertex (and count that # of odd is 0 or 2)
-  dfs(dfs, 0);
-  reverse(cycle.begin(), cycle.end());
-  if (cycle.size() != m) return {};
+  if (cycle.size() != m) return {}; // check that all edges are present in the cycle, fail otherwise
   return cycle;
 }
