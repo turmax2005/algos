62a065aa35e420b6a38facffe43f98c8e8881fab
No-Idle <leo.danilevich@mail.ru>
2024-09-10 20:27:56 +0300

    changed linear sieve

--- algos/linear-sieve.cpp
+++ algos/linear-sieve.cpp
@@ -1,11 +1,11 @@
-    const int C = 1e7+7;
-    vi pr, lp(C);
-    for (int i = 2; i < C; ++i) {
-        if (lp[i] == 0) {
-            lp[i] = i;
-            pr.app(i);
-        }
-        for (int j = 0; j < (int)pr.size() && pr[j] <= lp[i] && pr[j] * i < C; ++j) {
-            lp[pr[j] * i] = pr[j];
-        }
-    }
+const int C = 1e7+7;
+vi pr, lp(C);
+for (int i = 2; i < C; ++i) {
+  if (lp[i] == 0) {
+    lp[i] = i;
+    pr.app(i);
+  }
+  for (int j = 0; j < (int)pr.size() && pr[j] <= lp[i] && pr[j] * i < C; ++j) {
+    lp[pr[j] * i] = pr[j];
+  }
+}
