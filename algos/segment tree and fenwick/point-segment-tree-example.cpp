95f7f331d32a906c8453bc5e76f70134dfa730e7
Leonid Danilevich <58666971+No-Idle@users.noreply.github.com>
2024-12-08 15:28:56 +0300

    Update point-segment-tree-example.cpp

--- algos/segment tree and fenwick/point-segment-tree-example.cpp
+++ algos/segment tree and fenwick/point-segment-tree-example.cpp
@@ -1,9 +1,9 @@
 void example () {
   // max
-  SegmentTree<int> segtree(n, -(int)1e18,  [](int x, int y) { return max(x, y); });
+  SegmentTree segtree(n, -(int)1e18,  [](int x, int y) { return max(x, y); });
 
   // sum
-  SegmentTree<int> ones(n, 0LL,  [](int x, int y) { return x + y; });
+  SegmentTree ones(n, 0LL,  [](int x, int y) { return x + y; });
 
   auto left_zero = [&](int r) { // nearest zero strictly to the left
     return ones.firstTrue(r, [r](int sum, int l){ return r - l == sum; }) - 1;
