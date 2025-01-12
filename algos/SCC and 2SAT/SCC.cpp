d4e86fadb659da1193cd601abf2b1b2a259d9a93
Leonid Danilevich <58666971+No-Idle@users.noreply.github.com>
2025-01-12 15:44:42 +0300

    Update SCC.cpp

--- algos/SCC and 2SAT/SCC.cpp
+++ algos/SCC and 2SAT/SCC.cpp
@@ -1,28 +1,27 @@
 vector<int> find_scc(vector<vector<int>> g) {
-  vector<vector<int>> r(g.size());
-  for (int i = 0; i < g.size(); ++i) {
+  int n = g.size();
+  vector<vector<int>> r(n);
+  for (int i = 0; i < n; ++i) {
     for (int j : g[i]) r[j].push_back(i);
   }
-  vector<int> used(g.size()), tout(g.size());
+  vector<int> used(n), tout(n);
   int time = 0;
   auto dfs = [&](auto dfs, int cur) -> void {
-    if (used[cur]) return;
     used[cur] = 1;
     for (int nxt : g[cur]) {
-      dfs(dfs, nxt);
+      if (!used[nxt]) dfs(dfs, nxt);
     }
     tout[cur] = time++;
   };
-  for (int i = 0; i < g.size(); ++i) if (!used[i]) dfs(dfs, i);
-  vector<int> ind(g.size());
+  for (int i = 0; i < n; ++i) if (!used[i]) dfs(dfs, i);
+  vector<int> ind(n);
   iota(ind.begin(), ind.end(), 0);
   sort(all(ind), [&](int i, int j){return tout[i] > tout[j];});
-  vector<int> scc(g.size(), -1);
+  vector<int> scc(n, -1);
   auto go = [&](auto go, int cur, int color) -> void {
-    if (scc[cur] != -1) return;
     scc[cur] = color;
     for (int nxt : r[cur]) {
-      go(go, nxt, color);
+      if (scc[nxt] == -1) go(go, nxt, color);
     }
   };
   int color = 0;
