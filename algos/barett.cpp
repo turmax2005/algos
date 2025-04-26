26ae445b46d735223cce0b6e8065d1182da8a4e5
Leonid Danilevich <58666971+No-Idle@users.noreply.github.com>
2025-04-26 21:39:32 +0300

    Create barett.cpp

---
+++ algos/barett.cpp
@@ -0,0 +1,19 @@
+using u64 = unsigned long long;
+using u128 = __uint128_t;
+struct barrett{
+  u64 p, m;
+  barrett() {}
+  barrett(u64 p) : p(p), m(-1ULL / p) {}
+  u32 reduce(u64 x) {
+    u64 q = (u128(m) * x) >> 64, r = x - q * p;
+    return r - p * (r >= p);
+  }
+} BA;
+
+// Usage example:
+void solve() {
+  int p = ...;
+  ba = barett(p);
+  int x = ..., y = ...;
+  int prod = ba.reduce(x * y);
+}
