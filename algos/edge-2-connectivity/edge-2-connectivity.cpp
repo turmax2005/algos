d97864f95122a5e1dad48f8982dd91def7962b6e
Leonid Danilevich <58666971+No-Idle@users.noreply.github.com>
2025-08-12 23:15:09 +0300

    Update edge-2-connectivity.cpp

--- algos/edge-2-connectivity/edge-2-connectivity.cpp
+++ algos/edge-2-connectivity/edge-2-connectivity.cpp
@@ -1,78 +1,48 @@
-int n, m;
-cin >> n >> m;
-vector <vector <int> > g(n + 1);
-map <pair <int, int>, int> comp, col;
-for (int i = 0; i < m; ++i) {
-  int u, v, c; cin >> u >> v >> c;c--;
-  col[{u,v}]=col[{v,u}]=c;
-  g[u].push_back(v);
-  g[v].push_back(u);
-}
-vector <int> used(n + 1);
-vector <int> newCompWithoutParent(n + 1), h(n + 1), up(n + 1);
-auto findCutPoints = [&] (auto self, int u, int p) -> void {
-  used[u] = 1;
-  up[u] = h[u];
-  for (int v : g[u]) {
-    if (!used[v]) {
-      h[v] = h[u] + 1;
-      self(self, v, u);
-      up[u] = min(up[u], up[v]);
-      if (up[v] >= h[u]) {
-        newCompWithoutParent[v] = 1;
-      }
+    //n - number of vertices, m - number of edges, parallel edges -- ???, color of any edge is the color of its lower end
+vector <vector <int> > dfstree(n);
+vector <int> used(n), cut(n), h(n), up(n);
+auto findCutPoints = [&] (auto self, int u) -> void {
+    used[u] = 1;
+    up[u] = h[u];
+    for (int v : g[u]) {
+        if (!used[v]) {
+            dfstree[u].push_back(v);
+            h[v] = h[u] + 1;
+            self(self, v);
+            up[u] = min(up[u], up[v]);
+            if (up[v] >= h[u]) {
+                cut[v] = 1;
+            }
+        }
+        else {
+            up[u] = min(up[u], h[v]);
+        }
     }
-    else {
-      up[u] = min(up[u], h[v]);
-    }
-  }
 };
-for (int u = 1; u <= n; ++u) {
-  if (!used[u]) {
-    findCutPoints(findCutPoints, u, u);
-  }
-}
-int ptr = 0;
-vector <map <int, int> > colors(m);
-auto markComponents = [&] (auto self, int u, int cur) -> void {
-  used[u] = 1;
-  for (int v : g[u]) {
-    if (!used[v]) {
-      if (newCompWithoutParent[v]) {
-        ptr++;
-        self(self, v, ptr - 1);
-      }
-      else {
-        self(self, v, cur);
-      }
+findCutPoints(findCutPoints, 0);
+vector <vector <int> > tree(n + m);
+vector<int> color(n);color[0]=0;int ptr=n;
+auto build = [&] (auto self, int u) -> void {
+    for (int v : dfstree[u]) {
+        if (cut[v]) {
+            color[v]=ptr;++ptr;
+            self(self, v);
+        }
+        else {
+            color[v]=color[u];
+            self(self, v);
+        }
     }
-    else if (h[v] < h[u]) {
-      comp[{u,v}]=comp[{v,u}]=cur;
-      int c = col[{u,v}];
-      colors[cur][u] |= 1 << c;
-      colors[cur][v] |= 1 << c;
-    }
-  }
 };
-used.assign(n + 1, 0);
-for (int u = 1; u <= n; ++u) {
-  if (!used[u]) {
-    markComponents(markComponents, u, -1);
-  }
+build(build, 0);
+for(int i=0;i<n;++i) {
+    set<int> to;
+    for(int j:g[i]) {
+        int x=i,y=j;
+        if(h[x]<h[y]) swap(x,y);
+        to.insert(color[x]);
+    }
+    for(int j:to) {
+        tree[i].app(j);tree[j].app(i);
+    }
 }
-for (int comp = 0; comp < m; ++comp) {
-  vector <int> cnt(4);
-  int tot = 0;
-  for (auto [u, mask] : colors[comp]) {
-    tot |= mask;
-    cnt[bp(mask)]++;
-  }
-  if (bp(tot)<3) {
-    continue;
-  }
-  if (cnt[2] || cnt[3]>2) {
-    cout << "Yes" << endl;
-    return;
-  }
-}
-cout << "No" << endl;
