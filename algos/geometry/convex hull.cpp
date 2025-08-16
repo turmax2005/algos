33fd7008564b22c371f607483b8d8ccf36cb3747
leonid <leo.danilevich@mail.ru>
2025-08-16 23:22:38 +0300

    updated convex hull

--- algos/geometry/convex hull.cpp
+++ algos/geometry/convex hull.cpp
@@ -6,18 +6,18 @@ int cross(pt p, pt q) { return p.x * q.y - p.y * q.x; }
 pt operator-(pt a, pt b) { return {a.x - b.x, a.y - b.y}; }
 
 vector<point> convex(vector<point> a) {
-    sort(all(a));
-    a.erase(unique(all(a)), a.end());
-    vector<point> h;
-    for (int t = 0; t < 2; ++t) {
-        int sz = h.size() - t;
-        for (auto p: a) {
-            while (h.size() >= sz + 2 && cross(p - h.end()[-1], h.end()[-2] - h.end()[-1]) <= 0) h.pop_back();
-            h.push_back(p);
-        }
-        reverse(all(a));
+  sort(all(a));
+  a.erase(unique(all(a)), a.end());
+  vector<point> h;
+  for (int t = 0; t < 2; ++t) {
+    int sz = h.size() - t;
+    for (auto p: a) {
+      while (h.size() >= sz + 2 && cross(p - h.end()[-1], h.end()[-2] - h.end()[-1]) <= 0) h.pop_back();
+      h.push_back(p);
     }
-    return h; // h is circular: h.empty() || h.front() == h.back()
+    reverse(all(a));
+  }
+  return h; // h is circular: h.empty() || h.front() == h.back()
 }
 
 
