ab7335e5d3da27c933ccf69c1d586388d8d25bef
Максим Туревский <90309392+turmax2005@users.noreply.github.com>
2026-01-02 13:40:45 +0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,9 +2,92 @@
 
 using namespace std;
 #define int long long
-const int p=1e9+7;
-int po(int a,int b) {if(b==0) return 1; if(b==1) return a; if(b%2==0) {int u=po(a,b/2);return (u*1LL*u)%p;} else {int u=po(a,b-1);return (a*1LL*u)%p;}}
-int inv(int x) {return po(x,p-2);}
+template<int32_t MOD>
+struct ModInt {
+    int32_t value;
+
+    ModInt() : value(0) {}
+
+    ModInt(long long v) : value(v % MOD) { if (value < 0) value += MOD; }
+
+    ModInt(int32_t v): value(v % MOD) { if (value < 0) value += MOD; }
+
+    ModInt operator+=(ModInt m) {
+        value += m.value;
+        if (value >= MOD) value -= MOD;
+        return value;
+    }
+
+    ModInt operator-=(ModInt m) {
+        value -= m.value;
+        if (value < 0) value += MOD;
+        return value;
+    }
+
+    ModInt operator*=(ModInt m) {
+        value = (value * 1LL * m.value) % MOD;
+        return value;
+    }
+
+    ModInt power(long long exp) const {
+        if (exp == 0) return 1;
+        ModInt res = (exp & 1 ? value : 1);
+        ModInt half = power(exp >> 1);
+        return res * half * half;
+    }
+
+    ModInt operator/=(ModInt m) { return *this *= m.power(MOD - 2); }
+
+    friend std::istream &operator>>(std::istream &is, ModInt &m) {
+        is >> m.value;
+        return is;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const ModInt &m) {
+        os << m.value;
+        return os;
+    }
+
+    explicit operator int32_t() const { return value; }
+
+    explicit operator long long() const { return value; }
+
+    static int32_t mod() { return MOD; }
+};
+
+template<int32_t MOD>ModInt<MOD> operator+(ModInt<MOD> a, ModInt<MOD> b) { return a += b; }
+template<int32_t MOD, typename L>ModInt<MOD> operator+(L a, ModInt<MOD> b) { return ModInt<MOD>(a) += b; }
+template<int32_t MOD, typename R>ModInt<MOD> operator+(ModInt<MOD> a, R b) { return a += b; }
+
+template<int32_t MOD>ModInt<MOD> operator-(ModInt<MOD> a, ModInt<MOD> b) { return a -= b; }
+template<int32_t MOD, typename L>ModInt<MOD> operator-(L a, ModInt<MOD> b) { return ModInt<MOD>(a) -= b; }
+template<int32_t MOD, typename R>ModInt<MOD> operator-(ModInt<MOD> a, R b) { return a -= b; }
+
+template<int32_t MOD>ModInt<MOD> operator*(ModInt<MOD> a, ModInt<MOD> b) { return a *= b; }
+template<int32_t MOD, typename L>ModInt<MOD> operator*(L a, ModInt<MOD> b) { return ModInt<MOD>(a) *= b; }
+template<int32_t MOD, typename R>ModInt<MOD> operator*(ModInt<MOD> a, R b) { return a *= b; }
+
+template<int32_t MOD>ModInt<MOD> operator/(ModInt<MOD> a, ModInt<MOD> b) { return a /= b; }
+template<int32_t MOD, typename L>ModInt<MOD> operator/(L a, ModInt<MOD> b) { return ModInt<MOD>(a) /= b; }
+template<int32_t MOD, typename R>ModInt<MOD> operator/(ModInt<MOD> a, R b) { return a /= b; }
+
+template<int32_t MOD>bool operator==(ModInt<MOD> a, ModInt<MOD> b) { return a.value == b.value; }
+template<int32_t MOD, typename L>bool operator==(L a, ModInt<MOD> b) { return a == b.value; }
+template<int32_t MOD, typename R>bool operator==(ModInt<MOD> a, R b) { return a.value == b; }
+
+template<int32_t MOD>bool operator!=(ModInt<MOD> a, ModInt<MOD> b) { return a.value != b.value; }
+template<int32_t MOD, typename L>bool operator!=(L a, ModInt<MOD> b) { return a != b.value; }
+template<int32_t MOD, typename R>bool operator!=(ModInt<MOD> a, R b) { return a.value != b; }
+
+using mint = ModInt<998244353>;
+//using mint = ModInt<(int32_t)(1e9 + 7)>;
+//237c16
+const int maxn=2e6+5;
+mint fact[maxn];mint invf[maxn];
+void init() {
+    fact[0]=1;for(int i=1;i<maxn;++i) {fact[i]=(fact[i-1]*i);}
+    invf[maxn-1]=((mint) (1))/fact[maxn-1];for(int i=maxn-2;i>=0;--i) invf[i]=(invf[i+1]*(i+1));
+}
 mt19937 rnd;
 #define app push_back
 #define all(x) (x).begin(),(x).end()
