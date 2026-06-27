135facae1bfc6ab1555ee725ca821b9270fbd09f
Максим Туревский <90309392+turmax2005@users.noreply.github.com>
2026-06-27 12:56:32 +0300

    Create template-without-modint.cpp

---
+++ algos/template-without-modint.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+mt19937 rnd;
+#define app push_back
+#define all(x) (x).begin(),(x).end()
+#ifdef LOCAL
+#define debug(...) [](auto...a){ ((cout << a << ' '), ...) << endl;}(#__VA_ARGS__, ":", __VA_ARGS__)
+#define debugv(v) do {cout<< #v <<" : {"; for(int izxc=0;izxc<v.size();++izxc) {cout << v[izxc];if(izxc+1!=v.size()) cout << ","; }cout <<"}"<< endl;} while(0)
+#else
+#define debug(...)
+#define debugv(v)
+#endif
+#define print(x) cout<<(x)<<'\n'
+const int inf=1e18;
+pair<int,vector<int> > read()
+{
+    int n;cin>>n;vector<int> a(n);for(int &x:a) {cin>>x;} return {n,a};
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t;cin>>t;
+    while(t--)
+    {
+
+    }
+    return 0;
+}
